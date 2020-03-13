893e288fba6bfcad9de56306c02a33ca78950042
Antoine Eiche <lewo@abesis.fr>
2020-03-13 10:38:27 +0100

    Sources for all packages and not only for hello:/

--- scripts/swh-urls.nix
+++ scripts/swh-urls.nix
@@ -4,7 +4,7 @@ with builtins;
 let
   pkgs = import <nixpkgs> {};
   mirrors = import <nixpkgs/pkgs/build-support/fetchurl/mirrors.nix>;
-  urls = import <nixpkgs/maintainers/scripts/find-tarballs.nix> {expr = (import <nixpkgs/maintainers/scripts/all-tarballs.nix>).hello;};
+  urls = import <nixpkgs/maintainers/scripts/find-tarballs.nix> {expr = import <nixpkgs/maintainers/scripts/all-tarballs.nix>;};
   
   # If the url scheme is `mirror`, this translates this mirror to a real URL by looking in nixpkgs mirrors
   resolveMirrorUrl = url: with pkgs.lib; let
