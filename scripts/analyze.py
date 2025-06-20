f25cf4fb8ae38370b16db131ff691aad1ba076af
Antoine Lambert <anlambert@softwareheritage.org>
2025-06-20 16:01:35 +0200

    scripts/analyze.py: Improve formatting and update implementation
    Apply black formatting.
    
    Ensure to avoid errors after the update of sources data.

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -15,73 +15,72 @@ hosts = {}
 
 # To provide high level classification
 origin_patterns = {
-    'https?://hackage.haskell.org/.*': 'hackage',
-    'https?://github.com/(.*)/(.*)/archive/(.*)(?:.tar.gz|.zip)': 'github_archive', # noqa
-    'https?://github.com/(.*)/(.*)/releases/download/(.*)/(.*)': 'github_release', # noqa
-    'https://crates.io/api/v1/crates/(.*)/download': 'crates',
-    'https://bitbucket.org/(.*)/(.*)/(get|downloads)/(.*)(.tar.gz|.zip|tar.bz2)': 'bitbucket', # noqa
-    'https://rubygems.org/(.*)': 'rubygems',
-    'svn://(.*)': 'svn',
-    'https://(.*)/api/v4/projects/(.*)/repository/archive.tar.gz\?sha=(.*)': 'gitlab', # noqa
-    '.*': 'unknown',
+    "https?://hackage.haskell.org/.*": "hackage",
+    "https?://github.com/(.*)/(.*)/archive/(.*)(?:.tar.gz|.zip)": "github_archive",  # noqa
+    "https?://github.com/(.*)/(.*)/releases/download/(.*)/(.*)": "github_release",  # noqa
+    "https://crates.io/api/v1/crates/(.*)/download": "crates",
+    "https://bitbucket.org/(.*)/(.*)/(get|downloads)/(.*)(.tar.gz|.zip|tar.bz2)": "bitbucket",  # noqa
+    "https://rubygems.org/(.*)": "rubygems",
+    "svn://(.*)": "svn",
+    "https://(.*)/api/v4/projects/(.*)/repository/archive.tar.gz\\?sha=(.*)": "gitlab",  # noqa
+    ".*": "unknown",
 }
 
 extension_patterns = {
-    '.*(.tar.gz$|.zip$|tar.bz2$|.tbz$|.tar.xz$|.tgz|.tar)': 'archive',
-    '.*(.gem$)': 'gem',
-    '.*(.pom$)': 'pom',
-    '.*(.jar$)': 'jar',
-    '.*(.deb$)': 'deb',
-    '.*(.patch$)': 'patch',
-    '.*(.diff$)': 'diff',
-    '.*(.rpm$)': 'rpm',
-    '.*(.png$)': 'png',
-    '.*(.msi$)': 'msi',
-    '.*(.iso$)': 'iso',
-    '.*(.c$|.h$)': 'c',
-    '.*(.ttf$)': 'ttf',
-    '.*(.rock$)': 'rock',
-    '.*(.whl$)': 'whl',
-    '.*': 'unknown',
+    ".*(.tar.gz$|.zip$|tar.bz2$|.tbz$|.tar.xz$|.tgz|.tar)": "archive",
+    ".*(.gem$)": "gem",
+    ".*(.pom$)": "pom",
+    ".*(.jar$)": "jar",
+    ".*(.deb$)": "deb",
+    ".*(.patch$)": "patch",
+    ".*(.diff$)": "diff",
+    ".*(.rpm$)": "rpm",
+    ".*(.png$)": "png",
+    ".*(.msi$)": "msi",
+    ".*(.iso$)": "iso",
+    ".*(.c$|.h$)": "c",
+    ".*(.ttf$)": "ttf",
+    ".*(.rock$)": "rock",
+    ".*(.whl$)": "whl",
+    ".*": "unknown",
 }
 
-sources = j['sources']
+sources = j["sources"]
 for e in sources:
-
-    u = urlparse(e['urls'][0])
+    if e["type"] != "url" or not e["urls"]:
+        continue
+    u = urlparse(e["urls"][0])
     schemes[u.scheme] = schemes.get(u.scheme, 0) + 1
     hosts[u.netloc] = hosts.get(u.netloc, 0) + 1
 
     for k, v in origin_patterns.items():
-        if re.search(k, e['urls'][0]) is not None:
-            e['type'] = v
+        if re.search(k, e["urls"][0]) is not None:
+            e["type"] = v
             break
 
     for k, v in extension_patterns.items():
-        if re.search(k, e['urls'][0]) is not None:
-            e['file-type'] = v
+        if re.search(k, e["urls"][0]) is not None:
+            e["file-type"] = v
             break
 
 
 readme = """
-
-The file [`sources-{release}-full.json`](https://nix-community.github.io/nixpkgs-swh/sources-{release}-full.json)
-has been built from the [nixpkgs revision
-`{revision}`](https://github.com/NixOS/nixpkgs/tree/{revision}).
-This file contains `{sourceNumber}` sources, coming from
-`{hostNumber}` different hosts.
-
-The file [`sources-{release}.json`](https://nix-community.github.io/nixpkgs-swh/sources-{release}.json) is a filtered version which only contains archives. This file is consumed by SWH.
-
-"""
+The file [`sources-{release}.json`](https://nix-community.github.io/nixpkgs-swh/sources-{release}.json)
+has been built from the [nixpkgs revision `{revision}`](https://github.com/NixOS/nixpkgs/tree/{revision}).
+This file contains `{sourceNumber}` sources, coming from`{hostNumber}` different hosts.
+This file is consumed by SWH.
+"""  # noqa
 
 sortedHosts = sorted(hosts.items(), key=lambda h: h[1], reverse=True)
 
-print(readme.format(
-    revision=j['revision'],
-    release=j['release'],
-    sourceNumber=len(sources),
-    hostNumber=len(sortedHosts)))
+print(
+    readme.format(
+        revision=j["revision"],
+        release=j["release"],
+        sourceNumber=len(sources),
+        hostNumber=len(sortedHosts),
+    )
+)
 
 
 print("\n#### By host\n")
@@ -95,12 +94,10 @@ for k, v in schemes.items():
 
 types = {}
 file_types = {}
-fetchers = {}
 for s in sources:
-    types[s['type']] = types.get(s['type'], 0) + 1
-    file_types[s['file-type']] = file_types.get(s['file-type'], 0) + 1
-    if 'inferredFetcher' in s:
-        fetchers[s['inferredFetcher']] = fetchers.get(s['inferredFetcher'], 0) + 1
+    types[s["type"]] = types.get(s["type"], 0) + 1
+    if "file-type" in s:
+        file_types[s["file-type"]] = file_types.get(s["file-type"], 0) + 1
 
 print("\n#### By types\n")
 for k, v in types.items():
@@ -108,9 +105,4 @@ for k, v in types.items():
 
 print("\n#### By file types\n")
 for k, v in file_types.items():
-    print("     %16s %s" % (k, v))
-
-print("\n#### By fetchers\n")
-print("Be careful, fetchers are inferred and could be wrong\n")
-for k, v in fetchers.items():
     print("     %16s %s" % (k, v))
